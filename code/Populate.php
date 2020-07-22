7908eb96b1de86350f53c8d8addce9d33e6f1f9d
pjassem@xtm-intl.com <piotr.jassem@gmail.com>
2020-07-22 12:27:59 +0200

    PHP 7.2 / SilverStripe 3.7.x Support

--- code/Populate.php
+++ code/Populate.php
@@ -3,125 +3,129 @@
 /**
  * @package populate
  */
-class Populate extends Object {
-		
-	/**
-	 * @config
-	 *
-	 * @var array
-	 */
-	private static $include_yaml_fixtures = array();
+class Populate extends SS_Object
+{
 
-	/**
-	 * @config
-	 *
-	 * An array of classes to clear from the database before importing. While
-	 * populating sitetree it may be worth clearing the 'SiteTree' table.
-	 *
-	 * @var array
-	 */
-	private static $truncate_objects = array();
+    /**
+     * @config
+     *
+     * @var array
+     */
+    private static $include_yaml_fixtures = array();
 
-	/**
-	 * Flag to determine if we're already run for this session (i.e to prevent
-	 * parent calls invoking {@link requireRecords} twice).
-	 *
-	 * @var bool
-	 */
-	private static $ran = false;
+    /**
+     * @config
+     *
+     * An array of classes to clear from the database before importing. While
+     * populating sitetree it may be worth clearing the 'SiteTree' table.
+     *
+     * @var array
+     */
+    private static $truncate_objects = array();
 
-	/**
-	 * @var bool
-	 *
-	 * @throws Exception
-	 */
-	public static function requireRecords($force = false) {
-		if(self::$ran && !$force) {
-			return true;
-		}
+    /**
+     * Flag to determine if we're already run for this session (i.e to prevent
+     * parent calls invoking {@link requireRecords} twice).
+     *
+     * @var bool
+     */
+    private static $ran = false;
 
-		self::$ran = true;
+    /**
+     * @throws Exception
+     * @var bool
+     *
+     */
+    public static function requireRecords($force = false)
+    {
+        if (self::$ran && !$force) {
+            return true;
+        }
 
-		if(!(Director::isDev() || Director::isTest())) {
-			throw new Exception('requireRecords can only be run in development or test environments');
-		}
+        self::$ran = true;
 
-		$factory = Injector::inst()->create('PopulateFactory');
+        if (!(Director::isDev() || Director::isTest())) {
+            throw new Exception('requireRecords can only be run in development or test environments');
+        }
 
-		foreach(self::config()->get('truncate_objects') as $objName) {
-			$versions = array();
+        $factory = Injector::inst()->create('PopulateFactory');
 
-			if(class_exists($objName)) {
-				foreach(DataList::create($objName) as $obj) {
-					// if the object has the versioned extension, make sure we delete
-					// that as well
-					if($obj->hasExtension('Versioned')) {
-						foreach($obj->getVersionedStages() as $stage) {
-							$versions[$stage] = true;
+        foreach (self::config()->get('truncate_objects') as $objName) {
+            $versions = array();
 
-							$obj->deleteFromStage($stage);
-						}
-					}
+            if (class_exists($objName)) {
+                foreach (DataList::create($objName) as $obj) {
+                    // if the object has the versioned extension, make sure we delete
+                    // that as well
+                    if ($obj->hasExtension('Versioned')) {
+                        foreach ($obj->getVersionedStages() as $stage) {
+                            $versions[$stage] = true;
 
-					try {
-						@$obj->delete();
-					} catch(Exception $e) {
-						// notice
-					}
-				}
-			}
+                            $obj->deleteFromStage($stage);
+                        }
+                    }
 
-			if($versions) {
-				self::truncate_versions($objName, $versions);
-			}
+                    try {
+                        @$obj->delete();
+                    } catch (Exception $e) {
+                        // notice
+                    }
+                }
+            }
 
-			foreach((array)ClassInfo::getValidSubClasses($objName) as $table) {
-				self::truncate_table($table);
-				self::truncate_versions($table, $versions);
-			}
+            if ($versions) {
+                self::truncate_versions($objName, $versions);
+            }
 
-			self::truncate_table($objName);
-		}
+            foreach ((array)ClassInfo::getValidSubClasses($objName) as $table) {
+                self::truncate_table($table);
+                self::truncate_versions($table, $versions);
+            }
 
-		foreach(self::config()->get('include_yaml_fixtures') as $fixtureFile) {
-			$fixture = new YamlFixture($fixtureFile);
-			$fixture->writeInto($factory);
+            self::truncate_table($objName);
+        }
 
-			$fixture = null;
-		}
-			
-		// hook allowing extensions to clean up records, modify the result or
-		// export the data to a SQL file (for importing performance).
-		$static = !(isset($this) && get_class($this) == __CLASS__);
+        foreach (self::config()->get('include_yaml_fixtures') as $fixtureFile) {
+            $fixture = new YamlFixture($fixtureFile);
+            $fixture->writeInto($factory);
 
-		if($static) {
-			$populate = Injector::inst()->create('Populate');
-		} else {
-			$populate = $this;
-		}
+            $fixture = null;
+        }
 
-		$populate->extend('onAfterPopulateRecords');
+        // hook allowing extensions to clean up records, modify the result or
+        // export the data to a SQL file (for importing performance).
+        $static = !(isset($this) && get_class($this) == __CLASS__);
 
-		return true;
-	}
+        if ($static) {
+            $populate = Injector::inst()->create('Populate');
+        } else {
+            $populate = $this;
+        }
 
-	private static function truncate_table($table) {
-		DB::alteration_message("Truncating Table $table", "deleted");
+        $populate->extend('onAfterPopulateRecords');
 
-		if(ClassInfo::hasTable($table)) {
-			if(method_exists(DB::getConn(), 'clearTable')) {
-				DB::getConn()->clearTable($table);
-			} else {
-				DB::query("TRUNCATE \"$table\"");
-			}
-		}
-	}
+        return true;
+    }
 
-	private static function truncate_versions($table, $versions) {
-		self::truncate_table($table .'_versions');
-		
-		foreach($versions as $stage => $v) {
-			self::truncate_table($table . '_'. $stage);
-		}
-	}
+    private static function truncate_table($table)
+    {
+        DB::alteration_message("Truncating Table $table", "deleted");
+
+        if (ClassInfo::hasTable($table)) {
+            if (method_exists(DB::getConn(), 'clearTable')) {
+                DB::getConn()->clearTable($table);
+            } else {
+                DB::query("TRUNCATE \"$table\"");
+            }
+        }
+    }
+
+    private static function truncate_versions($table, $versions)
+    {
+        self::truncate_table($table . '_versions');
+
+        foreach ($versions as $stage => $v) {
+            self::truncate_table($table . '_' . $stage);
+        }
+    }
 }
